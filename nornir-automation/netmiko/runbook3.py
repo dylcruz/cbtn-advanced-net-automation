4ae9b737b1a6e28cafb7fba48c8ad1e32c0c7835
dylcruz <dylan@dylancruz.me>
2025-05-06 15:41:21 -0700

    nornir-netmiko examples

---
+++ nornir-automation/netmiko/runbook3.py
@@ -0,0 +1,13 @@
+from nornir import InitNornir
+from nornir_netmiko.tasks import netmiko_send_config
+from nornir_utils.plugins.functions import print_result
+
+commands = ['router eigrp 12', 'network 10.0.0.0', 'network 192.168.55.0']
+
+nr = InitNornir(config_file='config.yaml')
+
+def test_send_config_nm(task):
+    task.run(task=netmiko_send_config, config_commands=commands)
+
+results = nr.run(task=test_send_config_nm)
+print_result(results)
